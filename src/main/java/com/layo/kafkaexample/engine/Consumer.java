85cc0b45ae07bb4536fa5ef153d77d610df59b12
ValentinShilin <valentin.shilin.de@gmail.com>
2023-08-18 17:39:43 +0200

    spring

---
+++ src/main/java/com/layo/kafkaexample/engine/Consumer.java
@@ -0,0 +1,10 @@
+package com.layo.kafkaexample.engine;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
---
+++ src/main/java/com/layo/kafkaexample/engine/Producer.java
@@ -0,0 +1,1 @@
+package com.layo.kafkaexample.engine;
